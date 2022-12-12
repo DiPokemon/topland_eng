f6a97575eb6d304e9ee2d8f68fb01d9a1f61f0c8
Dimitrniy Nikolenko <dimitrnklnk@gmail.com>
2022-12-12 15:08:21 +0300

    перевод "О нас" + новые картинки

--- page-about.php
+++ page-about.php
@@ -8,7 +8,7 @@
 
     <section class="page__about first_block_about">
         <div class="_container">
-            <p class="page__about-text-block"><strong>Миссия нашего агентства</strong> - стремительно продвигать ваш бизнес в гору, брать новые высоты вместе с Вами и находить эффективные решения.</p>
+            <p class="page__about-text-block"><strong>The mission of our agency</strong> - is to rapidly promote your business uphill, take new heights together with you and find effective solutions.</p>
             <div class="page__about_wrapper">
 
                 <div class="page__about_wrapper-left">                    
@@ -16,47 +16,47 @@
                     <div class="about_item">
                         <span class="about_item-number">01</span> 
                         <div class="about_item-text"> 
-                            С помощью SEO выводим сайты на показатели до 70-90% собранных коммерческих ключевых запросов в ТОП-10
+                            With the help of SEO, we bring sites up to 70-90% of the collected commercial key queries in the TOP-10
                         </div>           
                     </div>
 
                     <div class="about_item">
                         <span class="about_item-number">02</span> 
                         <div class="about_item-text"> 
-                            Повышаем продажи с сайтов наших клиентов в 3 раза
+                            We increase sales from our clients' websites by 3 times
                         </div>           
                     </div>
 
                     <div class="about_item">
                         <span class="about_item-number">03</span>                             
                         <div class="about_item-text"> 
-                            Повышаем  доверие к бренду
+                            Increasing brand trust
                         </div>           
                     </div>
 
                     <div class="about_item">
                         <span class="about_item-number">04</span>                             
                         <div class="about_item-text"> 
-                            Улучшаем  техническое и информационное  состояния сайта
+                            We improve the technical and informational state of the site
                         </div>           
                     </div>
 
                     <div class="about_item">
                         <span class="about_item-number">05</span>
                         <div class="about_item-text"> 
-                            Мы выполнили  более 70 успешных SEO проектов
+                            We have completed over 70 successful SEO projects
                         </div>           
                     </div>
                     <div class="about_item">
                         <span class="about_item-number">06</span> 
                         <div class="about_item-text"> 
-                            Успешно реализовали более 15 проектов по созданию корпоративных сайтов
+                            Successfully implemented more than 15 projects to create corporate websites
                         </div>           
                     </div>
                 </div>
 
                 <div class="page__about_wrapper-right">
-                    <img loading="lazy" src="<?php echo get_template_directory_uri()?>/static/img/about_first.png" alt="Маркетинговое агентство в Ростове-на-Дону">
+                    <img loading="lazy" src="<?php echo get_template_directory_uri()?>/static/img/en_about_first.png" alt="Marketing agency">
                 </div>
         </div>
     </section>
@@ -66,37 +66,37 @@
             <div class="page__about_wrapper">
 
                 <div class="page__about_wrapper-left">
-                    <img loading="lazy" src="<?php echo get_template_directory_uri()?>/static/img/about_second.png" alt="SEO продвижение сайтов в Ростове">
+                    <img loading="lazy" src="<?php echo get_template_directory_uri()?>/static/img/en_about_second.png" alt="SEO Promotion">
                 </div>
                     
                 <div class="page__about_wrapper-right">
-                    <h2 class="page__about-block-title">Что мы умеем</h2>
+                    <h2 class="page__about-block-title">What can we do</h2>
 
                     <div class="about_item">
                         <span class="about_item-number">01</span> 
                         <div class="about_item-text"> 
-                            Разработка сайтов любой сложности с удобным интерфейсом, эстетичным внешним видом и практичным функционалом
+                            Development of websites of any complexity with a user-friendly interface, aesthetic appearance and practical functionality
                         </div>           
                     </div>
 
                     <div class="about_item">
                         <span class="about_item-number">02</span> 
                         <div class="about_item-text"> 
-                            SEO продвижение: по позициям, тематике, KPI, оффлайн магазинов, сайтов услуг и т.д.
+                            SEO promotion: by positions, topics, KPIs, offline stores, service sites, etc.
                         </div>           
                     </div>
 
                     <div class="about_item">
                         <span class="about_item-number">03</span> 
                         <div class="about_item-text"> 
-                            Работа с маркетплейсами: отзывы, контент, продвижение карточек товара в ТОП-10
+                            Working with marketplaces: reviews, content, promotion of product cards in the TOP-10
                         </div>           
                     </div>
 
                     <div class="about_item">
                         <span class="about_item-number">04</span>                             
                         <div class="about_item-text"> 
-                        Гео продвижение: размещение на картах Google Maps, Яндекс Картах и 2GIS
+                            Geo promotion: placement on Google Maps, Yandex Maps and 2GIS
                         </div>           
                     </div>                  
                 
@@ -109,83 +109,83 @@
             <div class="page__about_wrapper">
 
                 <div class="page__about_wrapper-left">
-                    <h2 class="page__about-block-title">Преимущества работы с нами</h2>
+                    <h2 class="page__about-block-title">Benefits of working with us</h2>
                     <div class="about_item">
                         <span class="about_item-number">01</span> 
                         <div class="about_item-text"> 
-                            Способствуем расширению сферы деятельности партнеров благодаря правильной маркетинговой стратегии
+                            We contribute to the expansion of the scope of partners' activities through the right marketing strategy
                         </div>           
                     </div>
 
                     <div class="about_item">
                         <span class="about_item-number">02</span> 
                         <div class="about_item-text"> 
-                            Работаем с технически сложными (узкоспециализированными) тематиками
+                            We work with technically complex (highly specialized) topics
                         </div>           
                     </div>
 
                     <div class="about_item">
                         <span class="about_item-number">03</span> 
                         <div class="about_item-text"> 
-                            Помогаем снизить затраты на рекламные бюджеты в 2 раза за 4 месяца работы при сохранении эффективности рекламной кампании
+                            We help to reduce the cost of advertising budgets by 2 times in 4 months of work while maintaining the effectiveness of the advertising campaign
                         </div>           
                     </div>
 
                     <div class="about_item">
                         <span class="about_item-number">04</span>                             
                         <div class="about_item-text"> 
-                            Публикуем результаты своих  работ с подробным  описанием процесса в разделе кейсы
+                            We publish the results of our work with a detailed description of the process in the case studies section
                         </div>           
                     </div>      
                     
                     <div class="about_item">
                         <span class="about_item-number">05</span>                             
                         <div class="about_item-text"> 
-                            Выполняем комплексную работу над проектом с заключением договора оказания услкуг
+                            We carry out complex work on the project with the conclusion of a contract for the provision of services
                         </div>           
                     </div>  
                     
                     <div class="about_item">
                         <span class="about_item-number">06</span>                             
                         <div class="about_item-text"> 
-                            Качественно решаем поставленные  задачи и предоставляем возможность дальнейшего развития
+                            We qualitatively solve the assigned tasks and provide an opportunity for further development
                         </div>           
                     </div>  
 
                     <div class="about_item">
                         <span class="about_item-number">07</span>                             
                         <div class="about_item-text"> 
-                            Сопровождаем  и консультируем клиента на всех этапах 
+                            We accompany and advise the client at all stages 
                         </div>           
                     </div> 
 
                     <div class="about_item">
                         <span class="about_item-number">08</span>                             
                         <div class="about_item-text"> 
-                            Исследуем рынки, вникаем в бизнес-процессы, строим гипотезы, тестируем – достигаем KPI
+                            We research markets, delve into business processes, build hypotheses, test - we achieve KPI
                         </div>           
                     </div> 
 
                     <div class="about_item">
                         <span class="about_item-number">09</span>                             
                         <div class="about_item-text"> 
-                            Используем исключительно современные технологии в веб-разработке, что гарантирует стабильную работу при высоких нагрузках
+                            We use exclusively modern technologies in web development, which guarantees stable operation under high loads
                         </div>           
                     </div> 
 
                     <div class="about_item">
                         <span class="about_item-number">10</span>                             
                         <div class="about_item-text"> 
-                            Предоставляем подробную отчетность о проделанной работе
+                            We provide detailed reporting on the work done
                         </div>           
                     </div> 
                 </div>
                     
                 <div class="page__about_wrapper-right">
-                    <img loading="lazy" src="<?php echo get_template_directory_uri()?>/static/img/about_thrid.png" alt="SEO продвижение сайтов в Ростове">
+                    <img loading="lazy" src="<?php echo get_template_directory_uri()?>/static/img/en_about_thrid.png" alt="SEO website promotion in Rostov">
                 </div>
         </div>
-        <p class="page__about-text-block">МАРКЕТИНГОВОЕ АГЕНТСТВО TOPLAND | РАБОТАЕМ В РОСТОВЕ НА ДОНУ С 2016 ГОДА</p>
+        <p class="page__about-text-block">MARKETING AGENCY TOPLAND | WORKING IN ROSTOV-ON-DON SINCE 2016</p>
     </section>
 
     <section class="page__contacts-map contact_block">
@@ -195,8 +195,8 @@
                     <script id="ymap_lazy" type="text/javascript" charset="utf-8" async data-src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A35c48d9808a50de74447c6d0ec48ba8b04881953d7f20f645b6e374170bb77f0&amp;width=100%25&amp;height=500&amp;lang=ru_RU&amp;scroll=true"></script>                                                    
                 </div>
                 <div class="form">
-                    <h3 class="form_title">У Вас остались вопросы</h3>
-                    <p class="form_text">Заполните форму обратной связи и наш специалист перезвонит Вам и проконсультирует по любой, интересующей Вас теме.</p>
+                    <h3 class="form_title">Do you have questions</h3>
+                    <p class="form_text">Fill out the feedback form and our specialist will call you back and advise on any topic that interests you.</p>
                     <div class="contact_form-column">
                     <?php echo do_shortcode('[contact-form-7 id="1968" title="Контактная форма 1"]'); ?>
 
